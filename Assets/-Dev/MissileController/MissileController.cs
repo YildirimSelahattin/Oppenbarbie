ce5c37456186fb77fa39ce141efa699f31321337
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-08-02 13:52:18 +0300

    Cam

--- Assets/-Dev/MissileController/MissileController.cs
+++ Assets/-Dev/MissileController/MissileController.cs
@@ -6,14 +6,13 @@ public class MissileController : MonoBehaviour
 {
     [SerializeField]
     [Range(0, 5000)]
-    private float _thrust = 10;
+    private float speed = 2;
     [SerializeField]
     private GameObject _target = null;
     [SerializeField]
     private float _maxAngularVelocity = 20;
     private Rigidbody _rb;
     Vector3 aimedDirection;
-    bool stop;
 
     void Start()
     {
@@ -21,34 +20,19 @@ public class MissileController : MonoBehaviour
         _rb.maxAngularVelocity = _maxAngularVelocity;
     }
 
-
     void Update()
     {
-        if (Input.touchCount > 0)
+        transform.Translate(Vector3.down * (speed * Time.deltaTime));
+        var targetDirection = (transform.position - _target.transform.position).normalized;
+        aimedDirection = targetDirection;
+
+        if (aimedDirection.x - transform.up.x < 0)
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                var targetDirection = (transform.position - _target.transform.position).normalized;
-                aimedDirection = targetDirection;
-                stop = false;
-            }
-            if (!stop)
-            {
-                if (aimedDirection.x - transform.up.x < 0)
-                {
-                    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z - 0.05f);
-
-                }
-                if (aimedDirection.x - transform.up.x > 0)
-                {
-                    transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z + 0.05f);
-                }
-            }
-            if (aimedDirection == -transform.up)
-            {
-                stop = true;
-            }
-
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z - 0.05f);
+        }
+        if (aimedDirection.x - transform.up.x > 0)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z + 0.05f);
         }
     }
 }
--- Assets/-Dev/MissileController/SwerveMovement.cs
+++ Assets/-Dev/MissileController/SwerveMovement.cs
@@ -3,63 +3,20 @@ using UnityEngine;
 
 public class SwerveMovement : MonoBehaviour
 {
-    [SerializeField] private float maxDisplacement = 0.2f;
-    [SerializeField] private float maxPositionX = 2f;
-    private Vector2 _anchorPosition;
+    public float speed = 5f; // Hareket hızı
 
-
-    private void Update()
+    void Update()
     {
-        var inputX = GetInput();
+        transform.Translate(Vector3.down * (2 * Time.deltaTime));
 
-        var displacementX = GetDisplacement(inputX);
-
-        displacementX = SmoothOutDisplacement(displacementX);
-        
-        var newPosition = GetNewLocalPosition(displacementX);
-
-        newPosition = GetLimitedLocalPosition(newPosition);
-
-        transform.localPosition = newPosition;
-
-       // transform.Translate(Vector3.down * (2 * Time.deltaTime));
-    }
-
-    private Vector3 GetLimitedLocalPosition(Vector3 position)
-    {
-        position.x = Mathf.Clamp(position.x, -maxPositionX, maxPositionX);
-        return position;
-    }
-    private Vector3 GetNewLocalPosition(float displacementX)
-    {
-        var lastPosition = transform.localPosition;
-        var newPositionX = lastPosition.x + displacementX;
-        var newPosition = new Vector3(newPositionX, lastPosition.y, lastPosition.z);
-        return newPosition;
-    }
-    private float GetInput()
-    {
-        var inputX = 0f;
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0)
         {
-            _anchorPosition = Input.mousePosition;
+            Touch touch = Input.GetTouch(0);
+            float horizontalInput = touch.position.x - Screen.width / 2f;
+            float movement = horizontalInput / (Screen.width / 2f);
+            transform.Translate(Vector3.right * -movement * speed * Time.deltaTime);
         }
-
-        else if (Input.GetMouseButton(0))
-        {
-            inputX = (Input.mousePosition.x - _anchorPosition.x);
-            _anchorPosition = Input.mousePosition;
-        }
-        return inputX;
-    }
-    private float GetDisplacement(float inputX)
-    {
-        var displacementX = 0f;
-        displacementX = inputX * Time.deltaTime;
-        return displacementX;
-    }
-    private float SmoothOutDisplacement(float displacementX)
-    {
-        return Mathf.Clamp(displacementX, -maxDisplacement, maxDisplacement);
     }
 }
+
+

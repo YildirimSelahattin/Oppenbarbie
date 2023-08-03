aa46e249a632f35a88bad86596c09008740fbe5d
oanld <omer.anildi@gmail.com>
2023-08-03 19:07:55 +0300

    missile attachment

--- Assets/Scripts/MissileProperties.cs
+++ Assets/Scripts/MissileProperties.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NozzlePosition : MonoBehaviour
+public class MissileProperties : MonoBehaviour
 {
     public enum NozzlePos { Left, Center, Right};
     public NozzlePos nozzlePosition;
-
+    public int partLevel;
     // Start is called before the first frame update
     void Start()
     {

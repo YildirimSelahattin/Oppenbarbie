a3453c4cca8614e60fad2e7bda03ba60e0d56674
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-08-09 13:38:38 +0300

    :end:

---
+++ Assets/-Dev/ProjectX/Bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Node"))
+        {
+            UIManager.Instance.restartButon.SetActive(true);
+            Destroy(MissileController.Instance.target, 1f);
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EndCam"))
+        {
+            GameManager.Instance.ChangeCamera(GameManager.Instance.endCam, 50);
+            StartCoroutine(DelayCam());
+        }
+        if (other.CompareTag("Finish"))
+        {
+            UIManager.Instance.restartButon.SetActive(true);
+            Destroy(MissileController.Instance.target, .5f);
+            Destroy(gameObject, .5f);
+        }
+    }
+
+    public IEnumerator DelayCam()
+    {
+        yield return new WaitForSeconds(2f);
+        GameManager.Instance.endCam.LookAt = null;
+        GameManager.Instance.endCam.Follow = null;
+    }
+}
